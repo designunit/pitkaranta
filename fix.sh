7787857fa766b5bd7583154a33a13ed751289542
Roman Timashev <roman@tmshv.ru>
2020-05-21 17:25:44 +0300

    change patch file

--- fix.sh
+++ fix.sh
@@ -3,7 +3,14 @@ echo "running fix.sh"
 # cat server_data.js | tr '\n' ' ' 
 # sed -i "72i" "public/index.html"
 head -n 71 readymag/index.html > public/index.html
-cat server_data.js | tr '\n' ' ' >> public/index.html
+
+# cat server_data.js | tr '\n' ' ' > temp_server_data
+# echo "\n" >> temp_server_data
+# cat temp_server_data >> public/index.html
+# rm temp_server_data 
+
+node server_data.js >> public/index.html
+
 tail -n +73 readymag/index.html >> public/index.html
 
 echo "done with fixing"
