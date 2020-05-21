54937cd7dd53d1bb6ac8d55a7517b7dc99d35329
Roman Timashev <roman@tmshv.ru>
2020-05-21 17:11:25 +0300

    fix tail upd

--- fix.sh
+++ fix.sh
@@ -4,6 +4,6 @@ echo "running fix.sh"
 # sed -i "72i" "public/index.html"
 head -n 71 readymag/index.html > public/index.html
 cat server_data.js | tr '\n' ' ' >> public/index.html
-tail +73 readymag/index.html >> public/index.html
+tail -n +73 readymag/index.html >> public/index.html
 
 echo "done with fixing"
